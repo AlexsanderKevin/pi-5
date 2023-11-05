5616bd6282f68d1b3087673331fc87ae456d408a
David Ordone <david9108@hotmail.com>
2023-11-05 19:08:22 -0300

    Funções de acesso a camera e leitura qrcode

--- src/pages/QrCode/index.tsx
+++ src/pages/QrCode/index.tsx
@@ -1,8 +1,40 @@
-import { StyleSheet, TextInput, TouchableOpacity, View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer';
+import Footer from '../../components/Footer/Footer'
 
-export default function QrCode({ navigation }) {
+import { BarCodeScanner } from 'expo-barcode-scanner'
+
+import { 
+  StyleSheet, 
+  TextInput, 
+  TouchableOpacity, 
+  View, 
+  Text 
+} from 'react-native'
+
+export default function QrCode({navigation}) {
+  const [ hasPermission, setHasPermission ] = useState(null)
+  const [ scanned, setScanned ] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
+  }, [])
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true)
+    alert(`Código QRCode tipo: ${type}, dados: ${data} escaneado`)
+    //TODO FUNCTION INSERT MOVEMENT 
+  }
+
+  if (hasPermission === null) {
+    alert(`Para utilizar esta função, conceda permissão de acesso a câmera nas configurações do seu smartphone.`)
+  }
+  if (!hasPermission) {
+    alert(`Sem permissão de acesso à câmera!`)
+  }
 
   return (
     <View style={styles.container}>
@@ -10,6 +42,10 @@ export default function QrCode({ navigation }) {
       <View>
         <Text>Qr Code</Text>
         <Text>{"{ Camera }"}</Text>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.camera}
+        />
       </View>
       <Footer navigation={navigation}/>
     </View>
@@ -20,10 +56,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 15,
-    backgroundColor: '#353535'
+    backgroundColor: '#353535',
+    justifyContent: 'center'
   },
   camera: {
-
+    height: '85%',
+    width: '100%',
+    top: 30
   }
-});
-
+})
